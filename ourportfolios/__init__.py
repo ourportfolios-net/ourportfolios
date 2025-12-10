31f8c7547c5c313ff73f787802e9e7fc0f4d4888
Minh Dang Hoang <hminhdang05@gmail.com>
2025-12-10 23:04:23 +0100

    chore: remove init file

--- ourportfolios/__init__.py
+++
@@ -1,6 +0,0 @@
-"""OurPortfolios application."""
-
-import warnings
-
-# Suppress pandas FutureWarning about 'M' frequency alias deprecation globally
-warnings.filterwarnings('ignore', category=FutureWarning, message=".*'M' is deprecated.*")
