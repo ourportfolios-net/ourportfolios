c846e9dd92af172fe451c46f5d1be3993e87a337
Minh Dang Hoang <hminhdang05@gmail.com>
2026-03-17 23:12:14 +0100

    fix: add motion to frontend for deployment

--- rxconfig.py
+++ rxconfig.py
@@ -2,7 +2,6 @@ import reflex as rx
 
 config = rx.Config(
     app_name="ourportfolios",
-    plugins=[
-        rx.plugins.TailwindV3Plugin(),
-        rx.plugins.sitemap.SitemapPlugin()],
+    plugins=[rx.plugins.TailwindV3Plugin(), rx.plugins.sitemap.SitemapPlugin()],
+    frontend_packages=["motion"],
 )
